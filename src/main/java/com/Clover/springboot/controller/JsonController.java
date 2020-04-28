f4fec4a9a38c57e529c8f2e43d1398ebd071b36e
michael50000 <methun50@gmail.com>
2020-04-28 22:48:12 +0530

    initial commit

---
+++ src/main/java/com/Clover/springboot/controller/JsonController.java
@@ -0,0 +1,57 @@
+package com.Clover.springboot.controller;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Clover.springboot.ExceptionHandler.StudentNotFoundException;
+import com.Clover.springboot.HibernateUtil.HibernateUtil;
+import com.Clover.springboot.Model.Student;
+
+@RestController
+public class JsonController {
+	
+	@RequestMapping(value = "/api/getStudents", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Student> getStudents() {
+	    
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+           return session.createQuery("from Student", Student.class).list();
+        }
+	}
+	
+	
+	@RequestMapping(value = "/api/getStudents/{sid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Student> getStudentsbyid(@PathVariable(value = "sid") Integer sid) {
+	    
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+           Query q= session.createQuery("select s from Student s where s.sid = :sid");
+           q.setParameter("sid", sid);
+           List<Student>st=q.list();
+           if(st!=null && st.size()<=0){
+        	   throw new StudentNotFoundException("Student: "+sid+ " Not Found" );
+           }
+            return st;
+        }
+	}
+	
+	/*@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<String> delete(@PathVariable("id") String id) {
+	   //repo.removeById(id);
+	return new ResponseEntity<>("Your message here", HttpStatus.OK);
+	    }
+	*/
+	
+
+
+}
+
+//Query q = getSession().createQuery("select u from User u where u.company.id = :id");
+//q.setParameter("id", id);
+//List<User> list = q.list();
